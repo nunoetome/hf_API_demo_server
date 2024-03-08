cd2ec74c8a9cbadc2440a54585e710d826377daf
Nuno Tomé <nunotomeit@gmail.com>
2024-03-08 10:35:21 +0000

    config json output

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def echo(text, request: gr.Request):
         output_text = json.dumps({"query_params": dict(request.query_params)})
     return output_text
 
-io = gr.Interface(echo, "textbox", "textbox").launch(share=True) 
+io = gr.Interface(echo, "textbox", "JSON").launch(share=True) 
 

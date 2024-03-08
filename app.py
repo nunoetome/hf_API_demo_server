d9d7bbc5d09303992b6a2c3eb0a48bf4bd960c4c
Nuno Tomé <nunotomeit@gmail.com>
2024-03-08 10:26:35 +0000

    done, to test

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import gradio as gr
+import json
 
 def echo(text, request: gr.Request):
+    output_text = json.dumps({"text": text})
     if request:
         print("Request headers dictionary:", request.headers)
+        output_text = json.dumps({"headers": dict(request.headers)})
         print("IP address:", request.client.host)
+        output_text = json.dumps({"headers": dict(request.headers)})
         print("Query parameters:", dict(request.query_params))
-    return text
+        output_text = json.dumps({"query_params": dict(request.query_params)})
+    return output_text
 
 io = gr.Interface(echo, "textbox", "textbox").launch(share=True) 
 

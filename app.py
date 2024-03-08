c9f7873b8e81926e6049ad3717d635ca5569a82c
Nuno Tomé <nunotomeit@gmail.com>
2024-03-08 10:45:27 +0000

    bug fix

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ def echo(text, request: gr.Request):
     output_text = json.dumps({"text": text})
     if request:
         print("Request headers dictionary:", request.headers)
-        output_text = json.dumps({"headers": dict(request.headers)})
+        output_text = json.add({"headers": dict(request.headers)})
         print("IP address:", request.client.host)
-        output_text = json.dumps({"headers": dict(request.headers)})
+        output_text = json.add({"headers": dict(request.headers)})
         print("Query parameters:", dict(request.query_params))
-        output_text = json.dumps({"query_params": dict(request.query_params)})
+        output_text = json.add({"query_params": dict(request.query_params)})
     return output_text
 
-io = gr.Interface(echo, "textbox", "json").launch(share=True) 
+io = gr.Interface(echo, "textbox", "JSON").launch(share=True) 
 

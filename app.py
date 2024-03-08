b62fcf987821f304f1731da1201077497daa9be7
Nuno Tomé <nunotomeit@gmail.com>
2024-03-08 10:16:39 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import gradio as gr
+
+def echo(text, request: gr.Request):
+    if request:
+        print("Request headers dictionary:", request.headers)
+        print("IP address:", request.client.host)
+        print("Query parameters:", dict(request.query_params))
+    return text
+
+io = gr.Interface(echo, "textbox", "textbox").launch(share=True) 
+

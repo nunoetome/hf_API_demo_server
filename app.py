09f606de62b37b31b1ae81168282d2c1b57623fe
Nuno Tomé <nunotomeit@gmail.com>
2024-03-09 20:15:05 +0000

    bug fix

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import json
 DEBUG_MODE = False
 
 def echo(text, request: gr.Request):
-    output_text = {"report1": SUCCESS}  # Initialize as a dictionary
-    output_text["report2"] = "You inserted the text: " 
+    output_text = {"report1": "SUCCESS"}  # Initialize as a dictionary
+    output_text["report2"] = "You inserted the text:" 
     output_text["report3"] = text
     if request:
         if DEBUG_MODE:
